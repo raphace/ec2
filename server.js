831d2e01f684ef5091b21599257cf4048a3f2894
Raphael <ott.raphael@gmail.com>
2013-11-10 18:59:13 +0100

    changed response type to text/html

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ http.createServer(function (request, response) {
 				'</div>' +
 				'</body>' +
 				'</html>';
-	response.writeHead(200, {'Content-Type': 'text/plain'});
+	response.writeHead(200, {'Content-Type': 'text/html'});
 	response.write(body);
 	response.end();
 
